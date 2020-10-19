499a3c205db53a3e39d652cd5835726045be0e2a
rendy fajar <Rendyfajar14@gmail.com>
2020-10-20 01:55:35 +0700

    login admin terbaru

--- routes/web.php
+++ routes/web.php
@@ -21,14 +21,18 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Auth::routes();
+// Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/home', 'HomeController@index')->name('home');
 
-Auth::routes();
+// Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/home', 'HomeController@index')->name('home');
 
-Auth::routes();
+// Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['prefix'=>'admin', 'middleware' =>'auth'], function() {
+    Route::get('/', 'Admin\DashboardController@index');
+});
